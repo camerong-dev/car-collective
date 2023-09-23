2459dec0533e7eadc130591d8b3c19c13d793a3d
camerong-dev <>
2023-09-23 10:17:29 +0000

    Add footer to page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import CollapsibleNav from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <BrowserRouter>
             <CollapsibleNav />
+            <Footer />
         </BrowserRouter>
     );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as brands from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+    return (
+        <footer className="py-3 my-4">
+            <Container>
+                <Row className="justify-content-center border-bottom pb-3 mb-3">
+                    <Col xs="auto" className="nav-item">
+                        <Nav.Link
+                            href="https://www.facebook.com"
+                            className="nav-link px-2 text-body-secondary"
+                            aria-label="Visit our Facebook page"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
+                        </Nav.Link>
+                    </Col>
+                    <Col xs="auto" className="nav-item">
+                        <Nav.Link
+                            href="https://www.instagram.com"
+                            className="nav-link px-2 text-body-secondary"
+                            aria-label="Visit our Instagram page"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <FontAwesomeIcon icon="fa-brands fa-square-instagram" />
+                        </Nav.Link>
+                    </Col>
+                    <Col xs="auto" className="nav-item">
+                        <Nav.Link
+                            href="https://www.reddit.com"
+                            className="nav-link px-2 text-body-secondary"
+                            aria-label="Visit our Reddit page"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <FontAwesomeIcon icon="fa-brands fa-square-reddit" />
+                        </Nav.Link>
+                    </Col>
+                    <Col xs="auto" className="nav-item">
+                        <Nav.Link
+                            href="https://www.twitter.com"
+                            className="nav-link px-2 text-body-secondary"
+                            aria-label="Visit our Twitter page"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            <FontAwesomeIcon icon="fa-brands fa-square-x-twitter" />
+                        </Nav.Link>
+                    </Col>
+                </Row>
+                <Row className="text-center nav-item text-body-secondary">
+                    <Col>
+                        <a href="https://github.com/camerong-dev">
+                            camerong-dev <FontAwesomeIcon icon="fa-brands fa-square-github" />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
+
+export default Footer;
