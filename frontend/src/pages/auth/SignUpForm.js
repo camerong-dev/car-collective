75da3abd2ab440026d8043e71f7f3e357663fabb
Cameron <c.gourley03@gmail.com>
2023-10-03 20:08:36 +0100

    Create SignUpForm page

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 function SignUp() {
-  return <h1>Hi</h1>;
+  const [email, setEmail] = useState("");
+  const [user_name, setUser_name] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email, "Username:", user_name, "Password:", password);
+  };
+
+  return (
+    <Container className="mt-5 registration-form">
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleSubmit} className="form">
+            <Form.Group controlId="formEmail" className="item">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formUsername" className="item">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                value={user_name}
+                onChange={(e) => setUser_name(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword" className="item">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="form-button-container">
+              <Button variant="primary" type="submit" className="form-button">
+                Register
+              </Button>
+            </Form.Group>
+          </Form>
+
+          <div className="mt-3 login-link">
+            <span>Already have an account?</span>
+            <a href="/signin">Sign in here</a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default SignUp;
