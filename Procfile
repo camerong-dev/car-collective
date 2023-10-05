31201295d4a6e53b75a0baf7d8a297429dca6301
Cameron <c.gourley03@gmail.com>
2023-10-05 20:05:28 +0100

    Import TemplateView into urls.py

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn core.wsgi
+release: python manage.py makemigrations && python manage.py migrate
--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from . import views
+from rest_framework import routers
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('collective.urls', namespace='collective')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api/', include('collective_api.urls', namespace='collective_api')),
     path('api/user/', include('accounts.urls', namespace='accounts')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
@@ -16,3 +18,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^.*$', views.index_view, name='index'),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
