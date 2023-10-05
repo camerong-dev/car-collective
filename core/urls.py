de07dc461b3aceb0d19e80f22606dab308fa359f
Cameron <c.gourley03@gmail.com>
2023-10-05 21:24:27 +0100

    More changes to settings.py

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,16 @@ from . import views
 from django.conf.urls import static
 from django.conf import settings
 from rest_framework import routers
+from django.shortcuts import render
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 from django.views.generic import TemplateView
+
+def render_react(request):
+    return render(request, "index.html")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,5 +22,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    re_path(r'^.*$', views.index_view, name='index'),
+    re_path(r"^$", render_react),
+    re_path(r"^(?:.*)/?$", render_react),
 ]
