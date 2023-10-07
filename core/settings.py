ce21726fcce1bdc99fc4a593a0783c52a7903b17
Cameron <c.gourley03@gmail.com>
2023-10-07 16:10:53 +0100

    Update cloudinary intergration

--- core/settings.py
+++ core/settings.py
@@ -147,8 +147,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'build', 'static'), ]
+STATIC_URL = 'car-collective/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static'), ]
 
 
 REST_FRAMEWORK = {
