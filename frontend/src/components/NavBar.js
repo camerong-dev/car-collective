a9914d2012fcbfd577423f0039724c707683d747
Cameron <c.gourley03@gmail.com>
2023-10-09 20:20:28 +0100

    Logic to get JWT token and get username of logged in user

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
 import { logoutUser } from "./Logout";
-
-const handleLogoutClick = () => {
-  logoutUser();
-};
+import jwtDecode from "jsonwebtoken/decode";
 
 function CollapsibleNav() {
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    if (isLoggedIn()) {
+      const token = localStorage.getItem("access_token");
+      const decodedToken = jwtDecode(token);
+      setUserName(decodedToken.user_name);
+    }
+  }, []);
+
+  const handleLogoutClick = () => {
+    logoutUser();
+    setUserName(null);
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
@@ -18,7 +30,7 @@ function CollapsibleNav() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link onClick={handleLogoutClick}>Log Out</Nav.Link>
+            <Nav.Link href="#">hi</Nav.Link>
             <Nav.Link href="#pricing">#</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -33,12 +45,27 @@ function CollapsibleNav() {
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="/signin">Sign In</Nav.Link>
+            {isLoggedIn() ? (
+              <NavDropdown
+                title={userName || "Loading..."}
+                id="collapsible-nav-dropdown"
+              >
+                <NavDropdown.Item onClick={handleLogoutClick}>
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav.Link href="/signin">Sign In</Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
+}
+
+function isLoggedIn() {
+  return !!localStorage.getItem("access_token");
 }
 
 export default CollapsibleNav;
