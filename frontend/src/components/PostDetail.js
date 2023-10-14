b94f9e304419ccf29ed6ef3d37de64feb13431ea
Cameron <c.gourley03@gmail.com>
2023-10-14 15:32:24 +0100

    Create and style detail post view

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../api/axiosDefaults";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import "../styles/PostDetail.css";
 
 function PostDetail() {
   const { id } = useParams();
@@ -10,13 +12,7 @@ function PostDetail() {
 
   useEffect(() => {
     axiosInstance
-      .get(`posts/${id}/`, {
-        headers: {
-          Authorization: localStorage.getItem("access_token")
-            ? "JWT " + localStorage.getItem("access_token")
-            : null,
-        },
-      })
+      .get(`posts/${id}/`)
       .then((response) => {
         setPost(response.data);
         setLoading(false);
@@ -33,10 +29,50 @@ function PostDetail() {
   if (!post) return <div>No post found</div>;
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>{post.author}</p>
-    </div>
+    <Container>
+      <Row className="mb-4">
+        <Col md={8}>
+          <div className="carousel-placeholder"></div>
+        </Col>
+
+        <Col md={4} className="data-col">
+          <Row className="mb-2">
+            <Col>{post.title}</Col>
+            <Col>{post.author}</Col>
+          </Row>
+          <Row className="mb-2">
+            <Col>more data</Col>
+            <Col>more date</Col>
+          </Row>
+          <Row className="mb-2">
+            <Col>more data</Col>
+            <Col>more date</Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <Row className="mb-4 description-section">
+        <Col md={8}>
+          <h4>Description:</h4>
+          <p>placeholder text...</p>
+        </Col>
+
+        <Col md={4} className="comments-section">
+          <h4>Comments:</h4>
+          <div className="mb-3">
+            <strong>placeholder author:</strong> fake comment!
+          </div>
+          <div className="mb-3">
+            <strong>placeholder author:</strong> another comment!
+          </div>
+          <div className="mb-3">
+            <strong>placeholder author:</strong> oooo comment!
+          </div>
+        </Col>
+      </Row>
+
+      <Row></Row>
+    </Container>
   );
 }
 
